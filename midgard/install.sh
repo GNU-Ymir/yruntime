407840711395ad8deea278317b19245eab42342e
EmileCadorel <ecadorel@gmail.com>
2018-01-22 16:01:13 +0100

    mise à jour de la procedure d'install

--- midgard/install.sh
+++ midgard/install.sh
@@ -1,8 +1,8 @@
 make
-echo | gyc -E -Wp,-v - &> files
-Y_INCLUDE_PATH=$(grep -A1 "here:" files | tail -n -1)/ymir/
-echo $Y_INCLUDE_PATH
-cp -r core/ $Y_INCLUDE_PATH
-cp -r std/ $Y_INCLUDE_PATH
-rm files
+sudo echo | gyc -E -Wp,-v - &> files
+Y_INCLUDE_PATH=$(sudo grep -A1 "here:" files | tail -n -1)/ymir/
+sudo echo $Y_INCLUDE_PATH
+sudo cp -r core/ $Y_INCLUDE_PATH
+sudo cp -r std/ $Y_INCLUDE_PATH
+sudo rm files
 make install
