7bdfc92310434e45b883c5cbca88d5d470fe3b5d
Emile Cadorel <ecadorel@gmail.com>
2018-02-12 21:34:00 +0100

    cmakelist pour midgard

--- midgard/install.sh
+++ midgard/install.sh
@@ -1,8 +1,7 @@
-make
 sudo echo | gyc -E -Wp,-v - &> files
 Y_INCLUDE_PATH=$(sudo grep -A1 "here:" files | tail -n -1)/ymir/
 sudo echo $Y_INCLUDE_PATH
 sudo cp -r core/ $Y_INCLUDE_PATH
 sudo cp -r std/ $Y_INCLUDE_PATH
 sudo rm files
-make install
+
