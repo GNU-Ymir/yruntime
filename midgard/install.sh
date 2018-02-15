8f2996014c17ceb8f57788b0b1b482616aca9e27
EmileCadorel <ecadorel@gmail.com>
2018-02-15 13:44:08 +0100

    correction install.sh

--- midgard/install.sh
+++ midgard/install.sh
@@ -1,5 +1,5 @@
 sudo echo | gyc -E -Wp,-v - &> files
-Y_INCLUDE_PATH=$(sudo grep -A1 "here:" files | tail -n -1)/ymir/
+Y_INCLUDE_PATH=$(sudo grep -A1 "<...>" files | tail -n -1)/ymir/
 sudo echo $Y_INCLUDE_PATH
 sudo cp -r core/ $Y_INCLUDE_PATH
 sudo cp -r std/ $Y_INCLUDE_PATH
