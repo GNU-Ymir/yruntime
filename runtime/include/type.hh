a9659ec344a109ecada3be185097e5a04c8970f0
Emile Cadorel <ecadorel@gmail.com>
2018-12-14 10:28:45 +0100

    mise à jour de la forme des vtable

--- runtime/include/type.hh
+++ runtime/include/type.hh
@@ -1,12 +1,13 @@
 #pragma once
 
 struct VTable;
-struct TypeInfo {
+struct TypeInfo {    
     VTable* vtable;
     void* ig1;
     void* ig2;
 };
   
 struct VTable {
+    TypeInfo* typeinfo;
     bool (*equals)(void*, TypeInfo);
 };
