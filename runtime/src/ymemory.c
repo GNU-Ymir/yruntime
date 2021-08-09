f061b78cd7129364d1f1617f82e47efcbaaf1bed
CADOREL Emile <ecadorel@gmail.com>
2021-08-09 09:47:01 +0200

    dcopy works on recursive values

--- runtime/src/ymemory.c
+++ runtime/src/ymemory.c
@@ -9,6 +9,10 @@
 /* #include <gc/gc_disclaim.h> */
 /* #endif */
 #include "gc.h"
+#include "../include/thread.h"
+
+pthread_mutex_t _yrt_dcopy_map_mutex;
+struct _yrt_dcopy_map_node _yrt_dcopy_head = {.len = 0, .used = 0, .from = NULL, .to = NULL};
 
 _yrt_array_ _yrt_dup_slice (_yrt_array_ arr, unsigned long size) {
     char* x = (char*) GC_malloc (arr.len * size);
@@ -88,3 +92,65 @@ _yrt_array_ _yrt_concat_slices (_yrt_array_ left, _yrt_array_ right, unsigned lo
     ret.len = left.len + right.len;
     return ret;
 }
+
+char _yrt_dcopy_map_is_started () {
+    _yrt_thread_mutex_lock (&_yrt_dcopy_map_mutex);
+    char ret = (_yrt_dcopy_head.len != 0);
+    _yrt_thread_mutex_unlock (&_yrt_dcopy_map_mutex);
+    return ret;
+}
+
+void _yrt_purge_dcopy_map () {
+    _yrt_thread_mutex_lock (&_yrt_dcopy_map_mutex);
+    _yrt_dcopy_head.len = 0;
+    _yrt_dcopy_head.used = 0;
+    _yrt_dcopy_head.from = NULL;
+    _yrt_dcopy_head.to = NULL;
+    _yrt_thread_mutex_unlock (&_yrt_dcopy_map_mutex);
+}
+
+void _yrt_insert_dcopy_map (void * data, void * to) {
+    _yrt_thread_mutex_lock (&_yrt_dcopy_map_mutex);
+    unsigned long index = _yrt_dcopy_head.used;
+    if (index >= _yrt_dcopy_head.len) _yrt_dcopy_map_grow ();
+
+    _yrt_dcopy_head.from [index] = data;
+    _yrt_dcopy_head.to [index] = to;
+    _yrt_dcopy_head.used += 1;
+    _yrt_thread_mutex_unlock (&_yrt_dcopy_map_mutex);
+}
+
+void* _yrt_find_dcopy_map (void * data) {
+    _yrt_thread_mutex_lock (&_yrt_dcopy_map_mutex);
+    for (unsigned long i = 0 ; i < _yrt_dcopy_head.used ; i++) {
+	if (_yrt_dcopy_head.from [i] == data) {
+	    _yrt_thread_mutex_unlock (&_yrt_dcopy_map_mutex);
+	    return _yrt_dcopy_head.to [i];
+	}
+    }
+    _yrt_thread_mutex_unlock (&_yrt_dcopy_map_mutex);
+    
+    return NULL;
+}
+
+
+void _yrt_dcopy_map_grow () {
+    unsigned long len = _yrt_dcopy_head.len;
+    if (len == 0) len = 10;
+    else len = len * 2;
+
+    void** from_res = (void**) GC_malloc (len * sizeof (void*));
+    void** to_res = (void**) GC_malloc (len * sizeof (void*));
+    
+    for (unsigned long i = 0 ; i < _yrt_dcopy_head.len ; i++) {
+	from_res [i] = _yrt_dcopy_head.from [i];
+	to_res [i] = _yrt_dcopy_head.to [i];
+    }
+
+    _yrt_dcopy_head.len = len;    
+    _yrt_dcopy_head.from = from_res;
+    _yrt_dcopy_head.to = to_res;
+}
+
+
+
