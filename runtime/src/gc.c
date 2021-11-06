31d6e90b29c2e3d60121d5b44b1fa758a0ad576c
Emile Cadorel <ecadorel@gmail.com>
2021-11-06 13:26:20 +0100

    update

---
+++ runtime/src/gc.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <string.h> 
+#include <sys/wait.h>
+#include <pthread.h>
+
+#define GC_THREADS
+#include "gc.h"
+#include <semaphore.h>
+
+void _yrt_disable_GC () {
+    GC_disable ();
+}
+
+void _yrt_enable_GC () {
+    GC_enable ();
+}
--- runtime/src/math.c
+++ runtime/src/math.c
@@ -1,5 +1,11 @@
 #include <math.h>
+#include <stdlib.h>
 
 float _yrt_sqrt (float x) {
     return sqrt (x);
+}
+
+
+void _yrt_init_rand () {
+    srand (time (NULL));
 }
