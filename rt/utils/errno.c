3f809eb7105a371147b2a5f27c8b52a5143604e8
Emile Cadorel <ecadorel@gmail.com>
2024-12-21 11:41:49 +0100

    feat: simple tcp stream to connect to ipv4 and ipv6 remote server

--- rt/utils/errno.c
+++ rt/utils/errno.c
@@ -1,4 +1,7 @@
 #include <errno.h>
+#include <string.h>
+#include "errno.h"
+#include <rt/utils/gc.h>
 
 int _yrt_get_errno () {
     return errno;
@@ -6,4 +9,20 @@ int _yrt_get_errno () {
 
 void _yrt_set_errno (int err) {
     errno = err;
+}
+
+_yrt_slice_t _yrt_str_get_errno (int err) {
+    char * errstr = strerror (err);
+    uint64_t len = strlen (errstr);
+    char * result = (char*) (GC_malloc (len + 1));
+
+    memcpy (result, errstr, len);
+    result [len] = 0;
+
+    _yrt_slice_t slc;
+    slc.len = len;
+    slc.data = result;
+    slc.blk_info = NULL;
+
+    return slc;
 }
--- rt/utils/errno.h
+++ rt/utils/errno.h
@@ -1,8 +1,11 @@
 #ifndef ERRNO_H_
 #define ERRNO_H_
 
+#include "../memory/types.h"
+
+
 int _yrt_get_errno ();
 void _yrt_set_errno (int err);
-
+_yrt_slice_t _yrt_str_get_errno (int err);
 
 #endif // ERRNO_H_
